28ee10a937ccb9040cca10d2a340406193ea8869
Kevin BESSON <kevin.besson.719@gmail.com>
2021-03-29 18:04:13 +0200

    Updated CSS

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import firebase from "firebase/app";
 
+import { Button, Grid } from "@material-ui/core";
+
 export default class SignIn extends Component {
   render() {
     return (
-      <div>
-        <button
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        justify='center'
+        style={{ minHeight: "100vh", fontSize: "1.5em" }}
+      >
+        <div style={{ color: "white", textAlign: "center" }}>
+          Please sign in to use the chat
+        </div>
+        <Button
+          variant='contained'
+          color='primary'
           onClick={() => {
             this.props.auth.signInWithPopup(
               new firebase.auth.GoogleAuthProvider()
             );
           }}
         >
-          You have to SIGN IN to use the chat
-        </button>
-      </div>
+          SIGN IN
+        </Button>
+      </Grid>
     );
   }
 }
